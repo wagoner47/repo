f4bc8f68db1e938779bc1ca8663b249ff34c0eeb
wagoner47 <wagoner47@email.arizona.edu>
2017-04-24 14:38:22 -0700

    Added first code

---
+++ code.py
@@ -0,0 +1,8 @@
+
+def do_something():
+    # complete
+    something = 'testing'
+    for i in range(1,4) :
+        something += ' {}'.format(i)
+    something += '...'
+    print(something)# this will make it much easier in future problems to see that something is actually happening
